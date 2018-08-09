6df37b5d93d37d64be7688fafc1b26cb87596fac
ahernandez <ahernandez@xolosa.com>
2018-08-09 15:24:24 -0600

    Reorganizar TypeScripts en carpetas

---
+++ ClassExamples/Section4/varScope.js
@@ -0,0 +1,9 @@
+"use strict";
+var a = 1;
+function test() {
+    console.log("Valor de a:");
+    console.log(a);
+}
+//Realizar prueba
+test();
+//# sourceMappingURL=varScope.js.map
---
+++ ClassExamples/Section4/varScope.ts
@@ -0,0 +1,8 @@
+let a: number = 1;
+function test(): void{
+    console.log("Valor de a:");
+    console.log(a);    
+}
+
+//Realizar prueba
+test();
