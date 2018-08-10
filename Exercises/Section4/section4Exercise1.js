5489797e814351a78cb4fc5a5028c534fd754272
Alvaro Hernandez <ahernandez@xolosa.com>
2018-08-10 14:02:18 -0600

    Ejercicios Seccion 4: Caracteristicas soportadas ES6

--- Exercises/Section4/section4Exercise1.js
+++ Exercises/Section4/section4Exercise1.js
@@ -1,29 +1,30 @@
 "use strict";
-console.log("Exercise 1 - Maybe use an Arrow Function?");
+console.log("Exercise 1 - Arrow functions");
 var double = function (value) { return value * 2; };
 console.log(double(10));
-console.log("Exercise 2 - If only we could provide some default values...");
+console.log("Exercise 2 - Default parameters");
 var greet = function (name) {
     if (name === void 0) { name = "Max"; }
     console.log("Hello, " + name);
 };
 greet();
 greet("Anna");
-console.log("Exercise 3 - Isn't there a shorter way to get all these Values?");
+console.log("Exercise 3 - Spread Operator");
 var numbers = [-3, 33, 38, 5];
 console.log(Math.min.apply(Math, numbers));
-console.log("Exercise 4 - I have to think about Exercise 3 ...");
+console.log("Exercise 4 - Spread Operator");
 var newArray = [55, 20];
 newArray.push.apply(newArray, numbers);
 console.log(newArray);
-console.log("Exercise 5 - That's a well-constructed array.");
+console.log("Exercise 5 - Destructuring Arrays");
 var testResults = [3.89, 2.99, 1.38];
 var result1 = testResults[0], result2 = testResults[1], result3 = testResults[2];
 console.log(result1, result2, result3);
-console.log("Exercise 6 - And a well-constructed object!");
+console.log("Exercise 6 - Destructuring Objects");
 var scientist = { firstName: "Will", experience: 12 };
 var firstName = scientist.firstName, experience = scientist.experience;
 console.log(firstName, experience);
+console.log("Template Literals");
 var studentName = 'Alvaro Hernandez';
 var messageTemplate = "Well it's done, thanks God it is.\nStudent: " + studentName;
 console.log(messageTemplate);
--- Exercises/Section4/section4Exercise1.ts
+++ Exercises/Section4/section4Exercise1.ts
@@ -1,34 +1,35 @@
-console.log("Exercise 1 - Maybe use an Arrow Function?"); 
-let double: (val:number) => number  = (value:number) => value * 2;
+console.log("Exercise 1 - Arrow functions"); 
+const double: (val:number) => number  = (value:number) => value * 2;
 console.log(double(10));
  
-console.log("Exercise 2 - If only we could provide some default values...");
-let greet = (name: string = "Max") => {    
-    console.log("Hello, " + name);
+console.log("Exercise 2 - Default parameters");
+const greet = (name: string = "Max") => {    
+    console.log(`Hello, ${name}`);
 };
 
 greet();
 greet("Anna");
  
-console.log("Exercise 3 - Isn't there a shorter way to get all these Values?");
+console.log("Exercise 3 - Spread Operator");
 let numbers: number[] = [-3, 33, 38, 5];
 console.log(Math.min(...numbers));
  
-console.log("Exercise 4 - I have to think about Exercise 3 ...");
+console.log("Exercise 4 - Spread Operator");
 let newArray: number[] = [55, 20];
 newArray.push(...numbers);
 console.log(newArray);
  
-console.log("Exercise 5 - That's a well-constructed array.");
+console.log("Exercise 5 - Destructuring Arrays");
 let testResults: number[] = [3.89, 2.99, 1.38];
 let [result1, result2, result3] = testResults;
 console.log(result1, result2, result3);
  
-console.log("Exercise 6 - And a well-constructed object!");
+console.log("Exercise 6 - Destructuring Objects");
 let scientist = {firstName: "Will", experience: 12};
 let {firstName, experience} = scientist;
 console.log(firstName, experience);
 
+console.log("Template Literals");
 let studentName: string ='Alvaro Hernandez';
 let messageTemplate=`Well it's done, thanks God it is.
 Student: ${studentName}`;
